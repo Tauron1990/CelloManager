01cabd521d3cd7fac5fa113b5c0a8e3d5bd6eb05
Alexander Bachmaier <Tauron.ab@gmail.com>
2022-07-04 13:17:55 +0200

    Commit

--- Game/RaiseOfNewWorld/Engine/Data/TextProcessing/ExpressionNode.cs
+++ Game/RaiseOfNewWorld/Engine/Data/TextProcessing/ExpressionNode.cs
@@ -1,6 +1,15 @@
 ﻿namespace RaiseOfNewWorld.Engine.Data.TextProcessing;
 
 public abstract class ExpressionNode<TResult>
-{
+{   
+    protected abstract class ExpressionParser
+    {
+        private readonly string _input;
+
+        protected ExpressionParser(string input) => _input = input;
+
+        protected abstract ExpressionNode<TResult> ParseFabricator(string input);
+    }
+
     public abstract TResult Evaluate(ViewContext context);
 }
