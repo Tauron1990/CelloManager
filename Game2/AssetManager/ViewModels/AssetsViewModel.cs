22b285a6e47c933f0351d26e3e86d8a02365dfa7
alexander <tauron.ab@gmail.com>
2022-08-10 19:50:11 +0200

    Commit

--- Game2/AssetManager/ViewModels/AssetsViewModel.cs
+++ Game2/AssetManager/ViewModels/AssetsViewModel.cs
@@ -9,9 +9,5 @@ namespace AssetManager.ViewModels;
 
 public sealed class AssetsViewModel : DataEntryViewModel
 {
-    protected override Task<DataEntry> AddEntry(string rootDic) => throw new NotImplementedException();
 
-    protected override Task EditContent(DataEntry entry) => throw new NotImplementedException();
-
-    protected override string ResolveFilePath(string root) => throw new NotImplementedException();
 }
--- Game2/AssetManager/ViewModels/ScriptsViewModel.cs
+++ Game2/AssetManager/ViewModels/ScriptsViewModel.cs
@@ -4,9 +4,5 @@ namespace AssetManager.ViewModels;
 
 public sealed class ScriptsViewModel : DataEntryViewModel
 {
-    protected override Task<DataEntry> AddEntry(string rootDic) => throw new System.NotImplementedException();
-
-    protected override Task EditContent(DataEntry entry) => throw new System.NotImplementedException();
-
-    protected override string ResolveFilePath(string root) => throw new System.NotImplementedException();
+ 
 }
