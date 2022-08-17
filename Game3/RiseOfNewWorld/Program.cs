537f5cdd93f19d654874dfb44e13890d24f6f308
Alexander <Tauron.ab@gmail.com>
2022-08-17 08:04:35 +0200

    Commit

--- Game3/RiseOfNewWorld/Program.cs
+++ Game3/RiseOfNewWorld/Program.cs
@@ -1,26 +1,48 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using EcsRx.Plugins.Views.Components;
-using EcsRx.Plugins.Views.Systems;
-using EcsRx.Plugins.Views.ViewHandlers;
+using System.Runtime.InteropServices;
 using Raylib_CsLo;
 
-const int screenWidth = 800;
-const int screenHeight = 450;
+namespace RiseOfNewWorld;
 
-Raylib.InitWindow(screenWidth, screenHeight, "Hallo Welt");
-
-
-while (!Raylib.WindowShouldClose())
+static class Program
 {
-    Raylib.BeginDrawing();
+    [DllImport("raylib", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [return: MarshalAs(UnmanagedType.U1)]
+    public static extern bool GuiSpinner(
+        Rectangle bounds,
+        [MarshalAs(UnmanagedType.LPStr)]
+        string text,
+        ref int value,
+        int minValue,
+        int maxValue,
+        [MarshalAs(UnmanagedType.U1)] 
+        bool editMode);
     
-    Raylib.ClearBackground(Raylib.DARKBLUE);
-    Raylib.DrawText("Hallo Welt", 190, 200, 20, Raylib.BLACK);
+    static int _test = 0;
     
-    Raylib.EndDrawing();
+    public static unsafe int Main(string[] args)
+    {
+        const int screenWidth = 800;
+        const int screenHeight = 450;
+
+        Raylib.InitWindow(screenWidth, screenHeight, "Hallo Welt");
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+
+            Raylib.ClearBackground(Raylib.DARKBLUE);
+            Raylib.DrawText("Hallo Welt", 190, 200, 20, Raylib.BLACK);
+
+            var spinnerText = "Test Spinner";
+            GuiSpinner(new Rectangle(100, 10, 100, 20), spinnerText , ref _test, 0, 20, true);
+            
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+
+        return 0;
+    }
 }
-
-Raylib.CloseWindow();
-
-return 0;
