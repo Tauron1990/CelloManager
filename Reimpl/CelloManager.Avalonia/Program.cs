a3c751eb8ed4b95d90e3647e1230cadb8521e901
Alexander Bachmaier <Tauron.ab@gmail.com>
2023-11-11 09:00:01 +0100

    Commit

--- Reimpl/CelloManager.Avalonia/Program.cs
+++ Reimpl/CelloManager.Avalonia/Program.cs
@@ -3,7 +3,6 @@ using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.ReactiveUI;
 using CelloManager.Core.Data;
-using QuestPDF.Fluent;
 using QuestPDF.Infrastructure;
 
 namespace CelloManager
@@ -14,12 +13,12 @@ namespace CelloManager
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
         [STAThread]
-        public static async Task Main(string[] args)
+        public static async ValueTask StartApp(string[] args)
         {
             QuestPDF.Settings.License = LicenseType.Community;
             Akavache.Registrations.Start("CelloManager");
          
-            await App.ServiceProvider.GetService<SpoolRepository>().Init();
+            await App.ServiceProvider.GetService<SpoolRepository>().Init().ConfigureAwait(true);
 
             try
             {
@@ -28,8 +27,8 @@ namespace CelloManager
             }
             finally
             {
-                await App.ServiceProvider.DisposeAsync();
-                await Akavache.BlobCache.Shutdown();
+                await App.ServiceProvider.DisposeAsync().ConfigureAwait(false);
+                await Akavache.BlobCache.Shutdown().ConfigureAwait(false);
             }
         }
 
